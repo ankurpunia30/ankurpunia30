8ce91eb12aaf091dd38f6a42da3ac53d5e47b567
Ankur <109734325+ankurpunia30@users.noreply.github.com>
2024-09-15 14:01:52 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -64,6 +64,15 @@ for(const [lineNumber,line] of lines.entries()){
     else if(ch==';'){
       tokens+='SEMICOLON ; null\n';
     }
+    else if(ch=='!'){
+      if(i+1<line.length &&line[i+1]=='='){
+        tokens+='BANG_EQUAL != null\n';
+        i++;
+      }
+      else{
+        tokens+='BANG ! null\n';
+      }
+    }
     else if(ch=='=' ){
       if(i+1<line.length &&line[i+1]=='='){
         tokens+='EQUAL_EQUAL == null\n';
