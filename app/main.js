53a2b0766e3822834342a2b940ab54e225c2bc89
Ankur <109734325+ankurpunia30@users.noreply.github.com>
2024-09-15 13:57:31 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -32,7 +32,7 @@ let lines=fileContent.split("\n");
 
 for(const [lineNumber,line] of lines.entries()){
   
-  for(let i in Range(0,line.length)){
+  for(let i ;i<line.length;i++){
     ch=line[i];
     if(ch=='('){
       tokens+='LEFT_PAREN ( null\n';
