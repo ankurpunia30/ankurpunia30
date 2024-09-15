1e5ba960a3fb4f19d0e455074b8e5361caf2ad92
Ankur <109734325+ankurpunia30@users.noreply.github.com>
2024-09-15 14:52:04 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -117,7 +117,7 @@ for(const [lineNumber,line] of lines.entries()){
       let num=0;
       
       while(i<line.length && line[i]>='0'&& line[i]<='9'){
-          num=num*10+parseInt(line[i]);
+          num=num*10+parseInt(line[i],10);
           i++;
         }
         let ans=parseFloat(num);
